e708e4d6008e95c600d1fb021332624254c56cc6
WIN-2RMTA8RDR3K\Administrator <1203675195@qq.com>
2023-06-12 15:53:29 +0800

    整理

--- webpack.config.js
+++ webpack.config.js
@@ -127,14 +127,14 @@ module.exports = {
     ]
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'engine'),
     // filename: '[name].js', //打包之后生成的文件名，可以随意写。
     filename: 'BIMEngine.js', //打包之后生成的文件名，可以随意写。
     library: 'BIMEngine', // 指定类库名,主要用于直接引用的方式(比如使用script 标签)
     libraryExport: "default", // 对外暴露default属性，就可以直接调用default里的属性
     globalObject: 'this', // 定义全局变量,兼容node和浏览器运行，避免出现"window is not defined"的情况
     libraryTarget: 'umd', // 定义打包方式Universal Module Definition,同时支持在CommonJS、AMD和全局变量使用
-    publicPath:"../../build/"
+    publicPath:"../../engine/"
   },
   plugins: [
     new CleanWebpackPlugin(),
